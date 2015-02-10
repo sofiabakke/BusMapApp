f347d584eed71835a54f1e43617954916f56dbeb
Sofia Nascimento Bakke <sofiabakke@gmail.com>
2015-02-10 15:35:27 +0100

    Tabbed view in StopsFragment
    Added a tabbed view to the StopsFragment buttons not connected yet

--- app/src/main/java/no/application/sofia/busmapapp/StopsFragment.java
+++ app/src/main/java/no/application/sofia/busmapapp/StopsFragment.java
@@ -3,9 +3,16 @@ package no.application.sofia.busmapapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Locale;
 
 
 /**
@@ -13,8 +20,10 @@ import android.view.ViewGroup;
  * Use the {@link StopsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StopsFragment extends Fragment {
+public class StopsFragment extends Fragment implements ActionBar.TabListener{
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private SectionsPagerAdapter sectionsPagerAdapter;
+    private ViewPager viewPager;
 
     /**
      * Use this factory method to create a new instance of
@@ -39,7 +48,12 @@ public class StopsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stops, container, false);
+        View view = inflater.inflate(R.layout.fragment_stops, container, false);
+        sectionsPagerAdapter = new SectionsPagerAdapter(getActivity().getSupportFragmentManager());
+
+        viewPager = (ViewPager)view.findViewById(R.id.pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        return view;
     }
 
     @Override
@@ -49,4 +63,101 @@ public class StopsFragment extends Fragment {
         ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER)); //Setting the Action Bar text
     }
 
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    /**
+     * A {@link android.support.v4.app.FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position){
+//                case 0:
+//                    Fragment placeholder = new PlaceholderFragment();
+//                    return placeholder;
+                case 0:
+                    Fragment fragment1 = new PlaceholderFragment2();
+                    return fragment1;
+                case 1:
+                    Fragment fragment2 = new PlaceholderFragment2();
+                    return fragment2;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+//                case 2:
+//                    return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment2 extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment2 newInstance(int sectionNumber) {
+            PlaceholderFragment2 fragment = new PlaceholderFragment2();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment2() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
 }
